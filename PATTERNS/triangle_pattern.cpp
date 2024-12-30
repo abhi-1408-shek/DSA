c343e706fb4be17e32d3af6140cd437950ef5aa6
Abhishek <abhishekpooja1408@gmail.com>
2024-12-30 16:12:11 +0530

    inverted triangle

--- PATTERNS/triangle_pattern.cpp
+++ PATTERNS/triangle_pattern.cpp
@@ -205,13 +205,14 @@ using namespace std;
 int main()
 {
     int n=4;
-    int num =1;
     for(int i=0;i<n;i++){
-         for(int j=n; j>i; j-- ){
-            cout<<num<<" ";
+        for(int j=0; j<=i; j++ ){
+            cout<<" ";
+        }
+        for(int j=n;j>i;j--){
+            cout<<i+1;
         }
         cout<<endl;
-        num++;
     }
  return 0;
 }
