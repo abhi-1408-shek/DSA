4349c12b998a4570d4b19e0ff261f1c42f8194f6
Abhishek <abhishekpooja1408@gmail.com>
2024-12-30 14:45:12 +0530

    adding

---
+++ PATTERNS/square_pattern.cpp
@@ -0,0 +1,182 @@
+
+/************************************************QUESTION-1************************************************/
+
+// 1234
+// 1234
+// 1234
+// 1234
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i=1;i<=4;i++){
+//         for(int j=1;j<=4;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-2************************************************/
+
+// 123
+// 456
+// 789
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {   int num = 1;
+//     for(int i=1; i<=3; i++){
+//         for(int j=1; j<=3;j++){
+//             cout<<num;
+//             num++;
+//         }
+       
+//         cout<<endl;
+//     }
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-3************************************************/
+
+// A B C 
+// D E F 
+// G H I 
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char c = 'A';
+//     for(int i=1;i<=3;i++){
+//         for(int j=1; j<=3;j++){
+//             cout<<c<<" ";
+//             c++;
+//         }
+//         cout<<endl;
+//     }
+
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-4************************************************/
+
+// *
+// **
+// ***
+// ****
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char c = '*';
+//     int n = 1;
+//     for(int i=0;i<4;i++){
+//         for(int j=0;j<n;j++){
+//             cout<<c;
+//         }
+//         cout<<endl;
+//         n++;
+//     }
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-5************************************************/
+
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//  int n = 1;
+//  int l=4;
+//  for(int i=0; i<l;i++){
+//     for(int j=0; j<=i; j++){
+//         cout<<n<<" ";
+//     }
+//     cout<<endl;
+//     n++;
+//  }
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-6************************************************/
+
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E 
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     char num = 'A';
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<=i;j++){
+//             cout<<num<<" ";
+//         }
+//         num++;
+//         cout<<endl;
+//     }
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-7************************************************/
+
+// 1
+// 12
+// 123
+// 1234
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<=i;j++){
+//             cout<<j+1;
+//         }
+//         cout<<endl;
+//     }
+//  return 0;
+// }
+
+
+/*************************************************QUESTION-8************************************************/
+
+//REVERSE TRIANGLE PATTERN
+
+
+#include<iostream>
+using namespace std;
+int main()
+{int n=4;
+    for(int i=0;i<n;i++){
+        
+    }
+ return 0;
+}
